2f77095bcad733ba676f7f4cfc64f41262c605f9
glorydark <darkteam@foxmail.com>
2021-06-26 14:05:01 +0800

    new Update! Version:1.0.2

--- src/main/java/glorydark/autosave/MainClass.java
+++ src/main/java/glorydark/autosave/MainClass.java
@@ -3,10 +3,12 @@ package glorydark.autosave;
 import cn.nukkit.Server;
 import cn.nukkit.event.Listener;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.plugin.PluginLogger;
 import cn.nukkit.utils.Config;
 import cn.nukkit.utils.TextFormat;
 import glorydark.autosave.utils.FileHandler;
 import glorydark.autosave.utils.GetCalender;
+import javafx.concurrent.Task;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,6 +19,9 @@ import java.util.TimeZone;
 public class MainClass extends PluginBase implements Listener {
     public static String path = null;
     public static Integer saveInterval = null;
+    public static PluginLogger logger;
+    public static Boolean save = false;
+    public static MainClass instance;
     @Override
     public void onLoad(){
         this.getLogger().info(TextFormat.YELLOW+"AutoSaveAndRestart onLoad!");
@@ -34,6 +39,8 @@ public class MainClass extends PluginBase implements Listener {
         int restartInterval = config.getInt("RestartIntervals", 60);
         this.getServer().getScheduler().scheduleDelayedTask(new AutoSaveTask(),saveInterval*120); //20ticks = 1s  1min = 60s -> 1min = 120ticks
         this.getServer().getCommandMap().register("保存文件夹",new SaveDataCommand("saveAllData"));
+        logger = this.getLogger();
+        instance = this;
     }
 
     @Override
@@ -47,21 +54,45 @@ public class MainClass extends PluginBase implements Listener {
     }
 
     public static void saveAllData(){
-        List<String> SaveDirPathList = MainClass.getSaveDirList();
-        for(String dirpath: SaveDirPathList){
-            String[] strings = dirpath.split("/");
-            File file = new File(MainClass.path+"/"+GetCalender.getDateString());
-            file.mkdir();
-            try {
-                FileHandler.copyDir(Server.getInstance().getDataPath()+"/"+dirpath,MainClass.path+"/"+GetCalender.getDateString() + "/" + strings[strings.length-1] + "/");
-            } catch (IOException e) {
-                e.printStackTrace();
+        if(save == true){
+            logger.warning("您还需要等待一段时间才可再次保存！");
+            return;
+        }else {
+            save = true;
+            List<String> SaveDirPathList = MainClass.getSaveDirList();
+            File file = new File(MainClass.path + "/" + GetCalender.getDateString());
+            for (String dirpath : SaveDirPathList) {
+                String[] strings = dirpath.split("/");
+                file.mkdir();
+                String savePath = MainClass.path + "/" + GetCalender.getDateString() + "/" + strings[strings.length - 1] + "/";
+                try {
+                    FileHandler.copyDir(Server.getInstance().getDataPath() + "/" + dirpath, savePath);
+                } catch (IOException e) {
+                    logger.error(TextFormat.GREEN + "复制文件夹失败，请尝试过几秒后重试!");
+                    e.printStackTrace();
+                    Server.getInstance().getScheduler().scheduleDelayedTask(instance, () -> {
+                        save = false;
+                        logger.info("您可以开始下一次保存了！");
+                    }, 20);
+                }
+                logger.info(TextFormat.GREEN + "保存文件夹【" + strings[strings.length - 1] + "】成功!");
             }
             try {
+                FileHandler.generateFile(MainClass.path + "/" + GetCalender.getDateString(), "zip");
+                logger.info(TextFormat.GREEN + "创建压缩文件【" + GetCalender.getDateString() + ".zip】成功!");
+                FileHandler.delete(file);
+                Server.getInstance().getScheduler().scheduleDelayedTask(instance, () -> {
+                    save = false;
+                    logger.info("您可以开始下一次保存了！");
+                }, 20);
             } catch (Exception e) {
+                logger.error(TextFormat.GREEN + "创建压缩文件失败，文件夹以为您保存到相应位置，请尝试过几秒后重试!");
+                Server.getInstance().getScheduler().scheduleDelayedTask(instance, () -> {
+                    save = false;
+                    logger.info("您可以开始下一次保存了！");
+                }, 20);
                 e.printStackTrace();
             }
         }
-        System.out.print("Save dictionary successfully!");
     }
 }
--- src/main/java/glorydark/autosave/utils/FileHandler.java
+++ src/main/java/glorydark/autosave/utils/FileHandler.java
@@ -1,17 +1,21 @@
 package glorydark.autosave.utils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.channels.FileLock;
+import cn.nukkit.utils.TextFormat;
+import glorydark.autosave.MainClass;
+
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class FileHandler {
+
+    private static final int  BUFFER_SIZE = 2 * 1024;
 
     public FileHandler() {
     }
 
     public static void copyDir(String fromDir,String toDir) throws IOException{
+        MainClass.logger.info(TextFormat.YELLOW+"复制目录"+fromDir+"成功!");
         //创建目录的File对象
         File dirSource = new File(fromDir);
         //判断源目录是不是一个目录
@@ -53,8 +57,6 @@ public class FileHandler {
         FileOutputStream out = new FileOutputStream(toFilePath);
         File fromFile = new File(fromFilePath);
         File toFile = new File(fromFilePath);
-        if(!fromFile.canRead()){ System.out.print("Failed to copy file:" +fromFilePath);return;}
-        if(!toFile.canWrite()){ System.out.print("Failed to write file:" +toFilePath); return;}
         //把读取到的内容写入新文件
         //把字节数组设置大一些   1*1024*1024=1M
         byte[] bs = new byte[1*1024*1024];
@@ -76,5 +78,70 @@ public class FileHandler {
             }
         }
         dir.delete();
+    }
+
+    public static void generateFile(String path, String format) throws Exception {
+
+        File file = new File(path);
+        // 压缩文件的路径不存在
+        if (!file.exists()) {
+            throw new Exception("路径 " + path + " 不存在文件，无法进行压缩...");
+        }
+        // 用于存放压缩文件的文件夹
+        String generateFile = file.getParent() + File.separator +"out";
+        File compress = new File(generateFile);
+        // 如果文件夹不存在，进行创建
+        if( !compress.exists() ){
+            compress.mkdirs();
+        }
+
+        // 目的压缩文件
+        String generateFileName = compress.getAbsolutePath() + File.separator + file.getName() + "." + format;
+
+        // 输入流 表示从一个源读取数据
+        // 输出流 表示向一个目标写入数据
+
+        // 输出流
+        FileOutputStream outputStream = new FileOutputStream(generateFileName);
+
+        // 压缩输出流
+        ZipOutputStream zipOutputStream = new ZipOutputStream(new BufferedOutputStream(outputStream));
+
+        generateFile(zipOutputStream,file,"");
+
+        MainClass.logger.info(TextFormat.YELLOW+"源文件位置：" + file.getAbsolutePath() + "，目的压缩文件生成位置：" + generateFileName);
+        // 关闭 输出流
+        zipOutputStream.close();
+    }
+
+    private static void generateFile(ZipOutputStream out, File file, String dir) throws Exception {
+
+        // 当前的是文件夹，则进行一步处理
+        if (file.isDirectory()) {
+            //得到文件列表信息
+            File[] files = file.listFiles();
+            //将文件夹添加到下一级打包目录
+            out.putNextEntry(new ZipEntry(dir + "/"));
+            dir = dir.length() == 0 ? "" : dir + "/";
+            //循环将文件夹中的文件打包
+            for (int i = 0; i < files.length; i++) {
+                generateFile(out, files[i], dir + files[i].getName());
+            }
+        } else { // 当前是文件
+
+            // 输入流
+            FileInputStream inputStream = new FileInputStream(file);
+            // 标记要打包的条目
+            out.putNextEntry(new ZipEntry(dir));
+            // 进行写操作
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = inputStream.read(bytes)) > 0) {
+                out.write(bytes, 0, len);
+            }
+            // 关闭输入流
+            inputStream.close();
+        }
+
     }
 }
